f09b4e42bcd11ac73489cbbd934ace4d46bde526
Manuel Flores <jflores@pcm.com.mx>
2023-06-06 08:45:23 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace RevisionPapeleraCorr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.dgvFABP = new System.Windows.Forms.DataGridView();
+            this.dgvCSV = new System.Windows.Forms.DataGridView();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFABP)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCSV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txtInfo.Location = new System.Drawing.Point(12, 12);
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ReadOnly = true;
+            this.txtInfo.Size = new System.Drawing.Size(308, 20);
+            this.txtInfo.TabIndex = 6;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(821, 11);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(242, 20);
+            this.btnGenerar.TabIndex = 5;
+            this.btnGenerar.Text = "GENERAR";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // dgvFABP
+            // 
+            this.dgvFABP.AllowUserToAddRows = false;
+            this.dgvFABP.AllowUserToDeleteRows = false;
+            this.dgvFABP.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvFABP.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvFABP.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvFABP.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFABP.Location = new System.Drawing.Point(12, 38);
+            this.dgvFABP.Name = "dgvFABP";
+            this.dgvFABP.ReadOnly = true;
+            this.dgvFABP.RowHeadersVisible = false;
+            this.dgvFABP.Size = new System.Drawing.Size(555, 400);
+            this.dgvFABP.TabIndex = 4;
+            // 
+            // dgvCSV
+            // 
+            this.dgvCSV.AllowUserToAddRows = false;
+            this.dgvCSV.AllowUserToDeleteRows = false;
+            this.dgvCSV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvCSV.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dgvCSV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCSV.Location = new System.Drawing.Point(574, 39);
+            this.dgvCSV.Name = "dgvCSV";
+            this.dgvCSV.ReadOnly = true;
+            this.dgvCSV.RowHeadersVisible = false;
+            this.dgvCSV.Size = new System.Drawing.Size(489, 399);
+            this.dgvCSV.TabIndex = 8;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(574, 11);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(241, 20);
+            this.btnBuscar.TabIndex = 9;
+            this.btnBuscar.Text = "BUSCAR";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1075, 450);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.dgvCSV);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.dgvFABP);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "FABP Pendientes de Enviar a Corr";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFABP)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCSV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.DataGridView dgvFABP;
+        private System.Windows.Forms.DataGridView dgvCSV;
+        private System.Windows.Forms.Button btnBuscar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace RevisionPapeleraCorr
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection con = null;
+        string Almacen, FABP, ftpAddress, username, password, FechaCreacion;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                buscar();
+                generar();
+
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                con.Close();
+                txtInfo.Text = "Mensaje: " + ex.ToString();
+            }
+}
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            buscar();
+        }
+
+        private void buscar()
+        {
+            dgvFABP.Refresh();
+
+            txtInfo.Text = "Buscando... ";
+
+            string strConnString = ConfigurationManager.ConnectionStrings["DB"].ConnectionString;
+            con = new SqlConnection(strConnString);
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            SqlCommand cmd = new SqlCommand();
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_BuscarFAPSPendientesCorr";
+            cmd.Connection = con;
+            cmd.CommandTimeout = 0;
+
+            Console.WriteLine(DateTime.Now);
+            adapter.Fill(dt);
+            Console.WriteLine(DateTime.Now);
+            dt.AcceptChanges();
+            dgvFABP.DataSource = dt;
+            con.Close();
+            txtInfo.Text = "Se termino la busqueda con " + dgvFABP.Rows.Count + " registros";
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            generar();
+        }
+
+        private void generar()
+        {
+            if(dgvFABP.Rows.Count > 0)
+            {
+                foreach (DataGridViewRow dgvRenglon in dgvFABP.Rows)
+                {
+                    Almacen = (dgvRenglon.Cells[1].Value.ToString());
+                    FABP = (dgvRenglon.Cells[2].Value.ToString());
+                    ftpAddress = (dgvRenglon.Cells[3].Value.ToString());
+                    username = (dgvRenglon.Cells[4].Value.ToString());
+                    password = (dgvRenglon.Cells[5].Value.ToString());
+                    FechaCreacion = (dgvRenglon.Cells[6].Value.ToString());
+
+                    dgvCSV.Refresh();
+
+                    txtInfo.Text = "Generando CSV " + FABP + " ... ";
+
+                    string strConnString = ConfigurationManager.ConnectionStrings["DB"].ConnectionString;
+                    con = new SqlConnection(strConnString);
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    SqlCommand cmd = new SqlCommand();
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "sp_GenerarCSVRevisionFABP";
+                    cmd.Parameters.Add("@Almacen", SqlDbType.VarChar).Value = Almacen;
+                    cmd.Parameters.Add("@FABP", SqlDbType.VarChar).Value = FABP;
+                    cmd.Connection = con;
+                    cmd.CommandTimeout = 0;
+
+                    Console.WriteLine(DateTime.Now);
+                    adapter.Fill(dt);
+                    Console.WriteLine(DateTime.Now);
+                    dt.AcceptChanges();
+                    dgvCSV.DataSource = dt;
+                    con.Close();
+                    txtInfo.Text = "Se termino la generacion del " + FABP + " con " + dgvCSV.Rows.Count + " registros";
+
+                    Enviar();
+                }
+            }            
+        }
+
+        private void Enviar()
+        {
+            try
+            {
+                if (dgvCSV.Rows.Count > 0)
+                {
+                    string rutaDirectorio = ConfigurationManager.AppSettings["rutaDirectorio"];
+                    string nombreArchivo = "CSV_" + FABP + "_" + Almacen + "_" + DateTime.Today.Day.ToString() + DateTime.Today.Month.ToString() + DateTime.Today.Year.ToString() + ".csv";
+
+                    txtInfo.Text = "Se creo el archivo " + nombreArchivo;
+
+                    TextWriter ArchivoDirectorio = new StreamWriter(rutaDirectorio + @"\" + nombreArchivo);
+
+                    txtInfo.Text = "PROCESANDO";
+                    for (int i = 0; i < dgvCSV.Rows.Count; i++)
+                    {
+                        string documento = Convert.ToString(dgvCSV.Rows[i].Cells["Rollos"].Value.ToString());
+                        ArchivoDirectorio.WriteLine($"{documento}");
+                        txtInfo.Text = i.ToString();
+                    }
+                    txtInfo.Text = "Se termino de llenar el archivo con " + dgvCSV.Rows.Count + " registros";
+                    ArchivoDirectorio.Close();
+                    dgvCSV.DataSource = null;
+                    dgvCSV.Rows.Clear();
+
+                    //EnvioFTP
+                    try
+                    {
+                        using (StreamReader stream = new StreamReader(rutaDirectorio + @"\" + nombreArchivo))
+                        {
+                            byte[] buffer = Encoding.Default.GetBytes(stream.ReadToEnd());
+                            WebRequest request = WebRequest.Create("ftp://" + ftpAddress + "/" + nombreArchivo);
+                            request.Method = WebRequestMethods.Ftp.UploadFile;
+                            request.Credentials = new NetworkCredential(username, password);
+                            Stream reqStream = request.GetRequestStream();
+                            reqStream.Write(buffer, 0, buffer.Length);
+                            reqStream.Flush();
+                            reqStream.Close();
+                        }
+                        WebRequest webreq = WebRequest.Create("ftp://" + ftpAddress + "/" + nombreArchivo);
+                        webreq.Method = WebRequestMethods.Ftp.GetFileSize;
+                        webreq.Credentials = new NetworkCredential(username, password);
+
+                        FtpWebResponse response = (FtpWebResponse)webreq.GetResponse();
+                        response.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        con.Close();
+                        txtInfo.Text = "Mensaje: " + ex.ToString();
+                    }
+                }
+                else
+                {
+                    txtInfo.Text = "Mensaje: Sin registros que procesar";
+                }
+            }
+            catch (Exception ex)
+            {
+                con.Close();
+                txtInfo.Text = "Mensaje: " + ex.ToString();
+            }
+        }
+    }
+}
